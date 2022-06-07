c803619a03ac6eafc93fef8fbb90d998f04091ab
Mack Gallagher <wingsonthebus@gmail.com>
2022-06-06 20:47:14 -0500

    meet MVP

--- api/server.js
+++ api/server.js
@@ -9,10 +9,10 @@ server.use(express.json());
 server.get('/api/users', (req, res) => {
   User.find()
     .then(result => {
-      res.json(result);
+      res.status(200).json(result);
     })
     .catch(err => {
-      res.json(err);
+      res.status(500).json(err);
     })
 });
 
@@ -20,48 +20,56 @@ server.get('/api/users/:id', (req, res) => {
   User.findById(req.params.id)
     .then(result => {
       if (!result) {
-        res.status(404).json({ message: 'user not found' });
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
         return;
       }
       res.json(result);
     })
     .catch(err => {
-      res.json(err);
+      res.status(500).json({ message: 'The users information could not be retrieved' });
     })
 });
 
 server.post('/api/users', (req, res) => {
+  if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('bio')) {
+    res.status(400).json({ message: 'Please provide name and bio for the user' });
+    return;
+  }
   User.insert(req.body)
     .then(result => {
-      res.status(201).json({ message: 'user created' });
+      res.status(201).json(result);
     })
     .catch(err => {
-      res.json(err);
+      res.status(500).json(err);
     })
 });
 
 server.put('/api/users/:id', (req, res) => {
+  if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('bio')) {
+    res.status(400).json({ message: 'Please provide name and bio for the user' });
+    return;
+  }
   User.update(req.params.id, req.body)
     .then(result => {
       if (!result) {
-        res.status(404).json({ message: 'user not found' });
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
         return;
       }
       res.json(result);
     })
     .catch(err => {
-      res.json(err);
+      res.status(500).json(err);
     })
 });
 
 server.delete('/api/users/:id', (req, res) => {
-  User.remove(id)
+  User.remove(req.params.id)
     .then(result => {
       if (!result) {
-        res.status(404).json({ message: 'user not found' });
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
         return;
       }
-      res.json(result);
+      res.status(200).json(result);
     })
 });
 
